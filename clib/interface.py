90bc414549759e71b3192cdf67c6847d2f575e6f
lensfa <mail@lensfa.com>
2024-07-14 12:00:24 +0800

    暂存

---
+++ clib/interface.py
@@ -0,0 +1,69 @@
+import ctypes
+from ctypes import cdll, POINTER, c_int, c_short, c_ubyte
+
+
+class FaseDetectInterface(object):
+    def __init__(self, lib_path: str):
+        # 建议输入绝对路径
+        self.base_lib = cdll.LoadLibrary(f'{lib_path}/libbase.so')
+        self.neon_lib = cdll.LoadLibrary(f'{lib_path}/libneon.so')
+
+        """
+        // 链接库调用函数
+        // unsigned char *result_buffer
+        // memory for storing face detection results, !!its size must be 0x800 Bytes!!
+
+        // input image, it must be BGR (three-channel) image!
+        short *facedetect_cnn(unsigned char *result_buffer, unsigned char *rgb_image_data, int width, int height, int step);
+        """
+
+        # 定义函数参数和返回值类型
+        self.base_lib.facedetect_cnn.argtypes = [
+            POINTER(c_ubyte),  # result_buffer
+            POINTER(c_ubyte),  # rgb_image_data
+            c_int,  # width
+            c_int,  # height
+            c_int  # step
+        ]
+        self.base_lib.facedetect_cnn.restype = POINTER(c_short)
+
+        self.neon_lib.facedetect_cnn.argtypes = [
+            POINTER(c_ubyte),  # result_buffer
+            POINTER(c_ubyte),  # rgb_image_data
+            c_int,  # width
+            c_int,  # height
+            c_int  # step
+        ]
+        self.neon_lib.facedetect_cnn.restype = POINTER(c_short)
+
+        # 分配结果缓冲区
+        self.result_buffer = (c_ubyte * 0x800)()
+
+    def detect_faces(self, image_array, method='base'):
+        if method == 'base':
+            lib = self.base_lib
+        elif method == 'neon':
+            lib = self.neon_lib
+        else:
+            raise ValueError("Unsupported Method")
+
+        width, height = image_array.shape[:2]
+        step = image_array.strides[0]
+
+        image_data = image_array.ctypes.data_as(POINTER(c_ubyte))
+        result_buffer = ctypes.cast(ctypes.create_string_buffer(0x800), POINTER(c_ubyte))
+
+        # 调用 C++ 函数
+        result_ptr = lib.facedetect_cnn(
+            result_buffer, image_data, width, height, step
+        )
+
+        # 获取结果数组长度
+        array_length = result_ptr[0]
+        print("得到结果数量: ", array_length)
+        if array_length > 0:
+            # 遍历结果数组
+            data_array: list = result_ptr[1: 16 * array_length]
+            for i in range(array_length):
+                result = data_array[i * 16: (1 + i) * 15]
+                print(result)
--- demo.py
+++ demo.py
@@ -1,46 +1,12 @@
-import ctypes
 import numpy as np
 from PIL import Image
 
-# 加载 C++ 库
-# 需要绝对路径
-lib = ctypes.cdll.LoadLibrary("/home/admin/py_ft/clib/build/lib/libbase.so")
-
-"""
-int *facedetect_cnn(unsigned char *result_buffer, unsigned char *rgb_image_data, int width, int height, int step);
-
-// unsigned char *result_buffer
-// memory for storing face detection results, !!its size must be 0x20000 Bytes!!
-
-// input image, it must be BGR (three-channel) image!
-"""
-
-# 定义函数参数和返回值类型
-lib.facedetect_cnn.argtypes = [
-    ctypes.POINTER(ctypes.c_ubyte),  # result_buffer
-    ctypes.POINTER(ctypes.c_ubyte),  # rgb_image_data
-    ctypes.c_int,  # width
-    ctypes.c_int,  # height
-    ctypes.c_int  # step
-]
-lib.facedetect_cnn.restype = ctypes.POINTER(ctypes.c_short)
+from clib.interface import FaseDetectInterface
 
 
 def detect_faces(image_path):
-    """
-    使用 C++ 函数检测人脸。
-
-    Args:
-        image_path: 图像路径。
-
-    Returns:
-        检测结果的 NumPy 数组。
-    """
-
-    # 使用 OpenCV 读取图像，注意 OpenCV 默认读取为 BGR 格式
     # 读取图像
     image = Image.open(image_path).convert("RGB")
-    width, height = image.size
 
     # 将图像转换为 BGR 格式
     image_np = np.array(image, dtype=np.uint8)
@@ -49,28 +15,8 @@ def detect_faces(image_path):
     image_np[:, :, 0] = c
     image_np[:, :, 2] = a
 
-    k = image_np.strides
-    step = image_np.strides[0]
-
-    image_data = image_np.ctypes.data_as(ctypes.POINTER(ctypes.c_ubyte))
-
-    # 分配结果缓冲区
-    result_buffer = (ctypes.c_ubyte * 0x800)()
-
-    # 调用 C++ 函数
-    result_ptr = lib.facedetect_cnn(
-        result_buffer, image_data, width, height, step
-    )
-
-    # 获取结果数组长度
-    array_length = result_ptr[0]
-    print("得到结果数量: ", array_length)
-    if array_length > 0:
-        # 遍历结果数组
-        data_array: list = result_ptr[1: 16*array_length]
-        for i in range(array_length):
-            result = data_array[i * 16: (1 + i) * 15]
-            print(result)
+    tool = FaseDetectInterface("/home/admin/py_ft/clib/build/lib")
+    tool.detect_faces(image_np)
 
 
 # 示例用法
